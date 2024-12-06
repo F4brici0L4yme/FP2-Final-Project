0568096f337ef442ecd148207c51d7b88ba62491
MauroSullcaMamani <msullcam@unsa.edu.pe>
2024-12-06 17:05:35 -0500

    Creando un metodo que reduce la vida del jugador

--- Jugador.java
+++ Jugador.java
@@ -10,4 +10,14 @@ public class Jugador {
         this.nombre=nombre;
     }
     
+    public void reducirVida(int cantidad) {
+        vida-=cantidad;
+        if (vida<=0){
+            vida=0;
+            estado=false;
+            System.out.println(nombre+" ha sido eliminado.");
+        }else{
+            System.out.println(nombre+" ahora tiene "+vida+" de vida.");
+        }
+    }
 }
