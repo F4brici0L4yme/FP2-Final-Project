cfbb9c374f68b1cda76ea1e6631dc3e74ab36037
MauroSullcaMamani <msullcam@unsa.edu.pe>
2024-12-06 17:01:09 -0500

    Creando algunos atributos de la clase jugador

--- Jugador.java
+++ Jugador.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ProyectoFinalLabs;
 
-/**
- *
- * @author Mauro Snayder
- */
 public class Jugador {
+    String nombre;
+    int vida=4;
+    boolean estado=true;
+    ArrayList<CajaObjetos> caja;
     
 }
