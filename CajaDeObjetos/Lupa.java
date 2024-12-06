9d3b2c13cf6cfd2192c6db1b95e2e8d1a068c6f1
Calihn1 <ehilacondob@unsa.edu.pe>
2024-12-06 18:22:13 -0500

    Mostrar si la bala es azul o rojo

--- CajaDeObjetos/Lupa.java
+++ CajaDeObjetos/Lupa.java
@@ -1,5 +1,21 @@
 package CajaJuego;
 
+import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
+
 public class Lupa {
 
+	public void UsarObjeto(Escopeta e) {
+		ArrayList<String> balas = e.RecuperarBalas();
+		String BalaMostrar = balas.get(0);
+			JFrame ventana = new JFrame();
+			ventana.setSize(200,200);
+			ventana.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		    ventana.setVisible(true);
+		    String frase1 = "La siguiente bala es azul";
+		    String frase2 = "La siguiente bala es roja";
+	}
+	
+	
 }
