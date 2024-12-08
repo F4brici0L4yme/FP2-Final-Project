6f488ffc2e5311e0ddb14d96c48742b0892d4002
Emanuel Hilacondo <ehilacondob@unsa.edu.pe>
2024-12-08 15:11:16 +0000

    Se integro la lógica donde se mostrará la bala siguiente si es azul es falsa y si es roja es verdadero, no tiene la opcion de eliminar

--- CajaDeObjetos/Lupa.java
+++ CajaDeObjetos/Lupa.java
@@ -4,18 +4,35 @@ import java.util.ArrayList;
 import javax.swing.*;
 import java.awt.*;
 
-public class Lupa {
+public class Lupa extends CajaDeObjetos{
 
 	public void UsarObjeto(Escopeta e) {
-		ArrayList<String> balas = e.RecuperarBalas();
-		String BalaMostrar = balas.get(0);
-			JFrame ventana = new JFrame();
-			ventana.setSize(200,200);
-			ventana.setLayout(new FlowLayout(FlowLayout.RIGHT));
-		    ventana.setVisible(true);
-		    String frase1 = "La siguiente bala es azul";
-		    String frase2 = "La siguiente bala es roja";
+		ArrayList<String> balas = e.getMunicion();
+		JFrame ventana = new JFrame();
+		ventana.setSize(400,100);
+		ventana.setLayout(new FlowLayout(FlowLayout.CENTER));
+		ventana.setVisible(true);
+		crearContenedor(ventana, balas);
 	}
 	
+	public void crearContenedor(JFrame ventana, ArrayList<String> balas) {
+		JTextField resultado;
+		String BalaMostrar = balas.get(0);
+		String frase1 = "La siguiente bala es azul";
+	    String frase2 = "La siguiente bala es roja";
+	    
+	    if(BalaMostrar.equals("roja")) {
+	    	resultado = new JTextField(frase2);
+	    	ventana.add(resultado);
+	    }
+	    else {
+	    	resultado = new JTextField(frase1);
+	    	ventana.add(resultado);
+	    }
+	}
 	
+	public static void main(String[]args) {
+		Escopeta escopeta = new Escopeta();
+		new Lupa().UsarObjeto(escopeta);
+	}
 }
