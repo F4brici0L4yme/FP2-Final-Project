860ad434786ce10ca60d79276c5217d3ba5e0bd3
Emanuel Hilacondo <ehilacondob@unsa.edu.pe>
2024-12-08 22:29:20 +0000

    Se implemento la logica de la adrenalina, que puede robar un objeto del rival, menos otra adrenalina, además se creó una ventana que mostrará todos los objetos como botones y cuando se presione uno se cerrará esta ventana y cuando pase veinte segundos se cierra

--- CajaDeObjetos/Adrenalina.java
+++ CajaDeObjetos/Adrenalina.java
@@ -1,5 +1,96 @@
-package CajaJuego;
+package CajaDeObjetos;
 
-public class Adrenalina {
+import java.util.*;
+import javax.swing.Timer;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class Adrenalina extends CajaDeObjetos {
+
+    private ArrayList<JButton> botones = new ArrayList<>();
+    private ArrayList<CajaDeObjetos> obs;
+    private ArrayList<CajaDeObjetos> misObs;
+    private JFrame ventana; 
+    private Timer cierreAutomatico;
+
+    public void recibirCajaDelJugador(ArrayList<CajaDeObjetos> obs) {
+        misObs = obs;
+    }
+
+    public void UsarObjeto(ArrayList<CajaDeObjetos> enemigo) {
+        obs = enemigo;
+        ventana = new JFrame();
+        ventana.setSize(700, 400);
+        crearContenedor(ventana);
+        ventana.setVisible(true);
+
+        cierreAutomatico = new Timer(20000, e -> ventana.dispose());
+        cierreAutomatico.setRepeats(false); 
+        cierreAutomatico.start(); 
+    }
+
+    public void crearContenedor(JFrame ventana) {
+        llenarBotones();
+        int cantObjetos = botones.size();
+        int posicion = 0;
+
+        ventana.setLayout(new GridLayout(2, 4, 7, 7));
+        for (JButton b : botones) {
+            ventana.add(b);
+            b.addActionListener(new Listener(posicion));
+            posicion++;
+        }
+    }
+
+    private class Listener implements ActionListener {
+        private int pos;
+
+        public Listener(int pos) {
+            this.pos = pos;
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            cierreAutomatico.stop();
+            misObs.add(obs.get(pos));
+            obs.remove(pos);
+            ventana.dispose();
+        }
+    }
+
+    public void llenarBotones() {
+        for (CajaDeObjetos ob : obs) {
+            JButton boton = null;
+
+            if (ob instanceof Lupa) {
+                boton = new JButton("Lupa");
+            } else if (ob instanceof Cigarro) {
+                boton = new JButton("Cigarro");
+            } else if (ob instanceof Sierra) {
+                boton = new JButton("Sierra");
+            } else if (ob instanceof Cerveza) {
+                boton = new JButton("Cerveza");
+            } else if (!(ob instanceof Adrenalina)) {
+                boton = new JButton("Esposa");
+            }
+
+            if (boton != null)
+                botones.add(boton);
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<CajaDeObjetos> objetos = new ArrayList<>();
+        objetos.add(new Lupa());
+        objetos.add(new Lupa());
+        objetos.add(new Cigarro());
+        objetos.add(new Sierra());
+        objetos.add(new Esposa());
+        objetos.add(new Cerveza());
+        objetos.add(new Cigarro());
+
+        Adrenalina adrenalina = new Adrenalina();
+        adrenalina.recibirCajaDelJugador(new ArrayList<>());
+        adrenalina.UsarObjeto(objetos);
+    }
 }
