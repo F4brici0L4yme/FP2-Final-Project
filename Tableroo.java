015094646cad38af53e79b86c1c909de36ebd2ee
F4brici0L4yme <rlaymes@unsa.edu.pe>
2024-12-13 15:27:23 -0500

    Flujo corregido & listo

--- Tableroo.java
+++ Tableroo.java
@@ -1,23 +1,30 @@
 import java.awt.*;
+import java.util.concurrent.CountDownLatch;
 import javax.swing.*;
 
-public class Tableroo extends JFrame{
+public class Tableroo extends JFrame {
     public ImageIcon shotgun = new ImageIcon("./images/shotgun.png");
     public ImageIcon corazonLleno = new ImageIcon("./images/vida.png");
+    public ImageIcon corazonRoto = new ImageIcon("./images/corazonRoto.png");
     public ImageIcon fondo = new ImageIcon("./images/bg.jpg");
     public ImageIcon imagenP1 = new ImageIcon("./images/p1.png");
     public ImageIcon imagenP2 = new ImageIcon("./images/p2.png");
     public ImageIcon balaAzulIcon = new ImageIcon("./images/balaAzul.png");
     public ImageIcon balaRojaIcon = new ImageIcon("./images/balaRoja.png");
     public ImageIcon cajaIcon = new ImageIcon("./images/cofre_cerrado.png");
+    Image imagenRedimensionada = cajaIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+    ImageIcon cajaRedimensionada = new ImageIcon(imagenRedimensionada);
     public JLabel imagenJugadorActual;
+    public JLabel nombreJugadorActual;
     public JPanel panelVidasJ1, panelVidasJ2;
     public JPanel panelInventario;
     public JTextArea logEventos;
+    public JScrollBar logEventos2v;
     public Escopeta escopeta = new Escopeta();
-    public boolean turnoJugador1 = true;
-    public Jugador jugador1 = new Jugador("P1");
-    public Jugador jugador2 = new Jugador("P2");
+    public boolean turnoJugador1 = false;
+    public Jugador jugador1 = new Jugador(JOptionPane.showInputDialog("Ingrese el nombre del jugador 1"));
+    public Jugador jugador2 = new Jugador(JOptionPane.showInputDialog("Ingrese el nombre del jugador 2"));
+    
     public Tableroo() {
         setTitle("Peruvian Roulette");
         setSize(800, 600);
@@ -32,40 +39,78 @@ public class Tableroo extends JFrame{
         JButton botonEscopeta = new JButton(shotgun);
         add(botonEscopeta, BorderLayout.CENTER);
         botonEscopeta.addActionListener(e -> mostrarOpcionesEscopeta());
+        modificarBotones(botonEscopeta);
+    
         /*Izquierda */
-        JPanel panelVidas = new JPanel(new GridLayout(2, 1));
-        panelVidasJ1 = new JPanel(new FlowLayout(FlowLayout.LEFT));/*LEFT hace que no se ajuste al centro y quede más chevere */
+        JPanel panelVidas = new JPanel(new GridLayout(4, 1));
+        panelVidasJ1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
         actualizarVidas(panelVidasJ1, jugador1.getVida());
+        panelVidas.add(new JLabel(jugador2.getNombre()));
         panelVidas.add(panelVidasJ1);
-
+    
         panelVidasJ2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
         actualizarVidas(panelVidasJ2, jugador2.getVida());
+        panelVidas.add(new JLabel(jugador1.getNombre()));
         panelVidas.add(panelVidasJ2);
-
+    
         add(panelVidas, BorderLayout.WEST);
+    
         /*Derecha */
-        JButton botonCaja = new JButton(cajaIcon);
-        botonCaja.addActionListener(e -> mostrarInventario(turnoJugador1 ? jugador1 : jugador2));
-        add(botonCaja, BorderLayout.EAST);
-
+        JPanel panelCajas = new JPanel(new GridLayout(4, 1));
+        JButton botonCaja = new JButton(cajaRedimensionada);
+        JButton botonCajaEnemiga = new JButton(cajaRedimensionada);
+    
+        botonCaja.addActionListener(e -> seleccionarInventario(turnoJugador1 ? jugador1 : jugador2));
+        botonCajaEnemiga.addActionListener(e -> mostrarInventario(turnoJugador1 ? jugador2 : jugador1));
+        panelCajas.add(new JLabel("Caja Enemiga"));
+        panelCajas.add(botonCajaEnemiga);
+        panelCajas.add(new JLabel("Mi caja"));
+        panelCajas.add(botonCaja);
+        modificarBotones(botonCaja);
+        modificarBotones(botonCajaEnemiga);
+        add(panelCajas, BorderLayout.EAST);
+    
         /*Abajo */
         JPanel panelInferior = new JPanel(new BorderLayout());
-
+    
         imagenJugadorActual = new JLabel(imagenP1);
-        panelInferior.add(imagenJugadorActual, BorderLayout.WEST);
-
+        nombreJugadorActual = new JLabel(jugador1.getNombre());
+        JPanel imagenYnombre = new JPanel();
+        imagenYnombre.setLayout(new BoxLayout(imagenYnombre, BoxLayout.Y_AXIS));
+        imagenYnombre.add(nombreJugadorActual);
+        imagenYnombre.add(imagenJugadorActual);
+    
+        imagenYnombre.setPreferredSize(new Dimension(150, 80));
+        panelInferior.add(imagenYnombre, BorderLayout.WEST);
+    
         logEventos = new JTextArea(5, 30);
         logEventos.setEditable(false);
-        panelInferior.add(logEventos, BorderLayout.CENTER);
-        add(panelInferior, BorderLayout.SOUTH);
-        mostrarInformacionEscopeta(logEventos);
-        
-    }
+        logEventos.setLineWrap(true);
+        logEventos.setWrapStyleWord(true);
 
+        JScrollPane scrollLogEventos = new JScrollPane(logEventos);
+        scrollLogEventos.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        panelInferior.add(scrollLogEventos, BorderLayout.CENTER);
+        repartirCajaJugador(jugador1);
+        repartirCajaJugador(jugador2);
+        add(panelInferior, BorderLayout.SOUTH);
+
+        mostrarInformacionEscopeta(logEventos);
+    }
+    
+    public void agregarTextoLog(String texto) {
+        logEventos.append(texto + "\n");
+        logEventos.setCaretPosition(logEventos.getDocument().getLength());
+    }
+    
     public void actualizarVidas(JPanel panelVidas, int vidas) {
         panelVidas.removeAll();
         for (int i = 0; i < vidas; i++) {
             JLabel corazon = new JLabel(corazonLleno);
+            panelVidas.add(corazon);
+        }
+        for(int i = 0; i< 4 - vidas; i++){
+            JLabel corazon = new JLabel(corazonRoto);
             panelVidas.add(corazon);
         }
         panelVidas.revalidate();
@@ -79,60 +124,47 @@ public class Tableroo extends JFrame{
                 JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE,
                 null, opciones, opciones[0]);
 
+        String balaDisparada = escopeta.getMunicion().get(0);
+        boolean balaEsReal = balaDisparada.equals("roja");
+    
+        Jugador objetivo = null;
         if (respuesta == 0) {
-            if (turnoJugador1) {
-                if(escopeta.getMunicion().get(0).equals("rojo")){
-                    escopeta.disparar(jugador2);
-                    logEventos.append("Disparaste a J2");
-                }
-                else{
-                    logEventos.append("Era una bala falsa...");
-                }
+            objetivo = turnoJugador1 ? jugador2 : jugador1;
+            if (balaEsReal) {
+                escopeta.disparar(objetivo);
+                logEventos.append("Fue una bala verdadera...\n");
             } else {
-                if(escopeta.getMunicion().get(0).equals("rojo")){
-                    escopeta.disparar(jugador1);
-                    logEventos.append("Disparaste a J1");
-                }
-                else{
-                    logEventos.append("Era una bala falsa...");
-                }
+                logEventos.append("Era una bala falsa...\n");
             }
         } else if (respuesta == 1) {
-            if (turnoJugador1) {
-                if(escopeta.getMunicion().get(0).equals("rojo")){
-                    escopeta.disparar(jugador1);
-                    logEventos.append("Era una bala verdadera...");
-                }
-                else{
-                    logEventos.append("Era una bala falsa...¡Ganas un turno por tu valentía!");
-                }
+            objetivo = turnoJugador1 ? jugador1 : jugador2;
+            if (balaEsReal) {
+                escopeta.disparar(objetivo);
+                logEventos.append("Fue una bala verdadera...\n");
             } else {
-                if(escopeta.getMunicion().get(0).equals("rojo")){
-                    escopeta.disparar(jugador2);
-                    logEventos.append("Era una bala verdadera...");
-                }
-                else{
-                    logEventos.append("Era una bala falsa...¡Ganas un turno por tu valentía!");
-                }
+                logEventos.append("Era una bala falsa... ¡Ganas un turno por tu valentía!\n");
+                /*Añadir sonido de bala falsa */
             }
         }
-        if(escopeta.getMunicion().get(0).equals("rojo") || !jugador1.estaEsposado())
-            cambiarTurno();
-        else if(jugador1.estaEsposado()) {
-            jugador1.setEstaEsposado(false);
+        escopeta.getMunicion().remove(0);
+        if (respuesta != 1 || balaEsReal) {
+            if(!objetivo.estaEsposado()){
+                cambiarTurno();
+            }
         }
         actualizarVidas(panelVidasJ1, jugador1.getVida());
         actualizarVidas(panelVidasJ2, jugador2.getVida());
-        if(escopeta.getMunicion().isEmpty()){
-            logEventos.append("Fin de la ronda...");
-            escopeta.cargarBalas();
-            for(int i = 0; i<escopeta.getMunicion().size(); i++){
-                logEventos.append(escopeta.getMunicion().get(i));
-            }
-        }
         verificarGanador();
-
+        if (escopeta.getMunicion().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Ronda terminada...\n");
+            escopeta.cargarBalas();
+            logEventos.append("Nuevas balas insertadas:\n");
+            mostrarInformacionEscopeta(logEventos);
+            repartirCajaJugador(jugador1);
+            repartirCajaJugador(jugador2);
+        }
     }
+    
     public void verificarGanador() {
         if (jugador1.getVida() <= 0 || jugador2.getVida() <= 0) {
             String ganador = (jugador1.getVida() > 0) ? jugador1.getNombre() : jugador2.getNombre();
@@ -140,61 +172,133 @@ public class Tableroo extends JFrame{
             System.exit(0);
         }
     }
+    
     public void cambiarTurno() {
         turnoJugador1 = !turnoJugador1;
         if (turnoJugador1){
             imagenJugadorActual.setIcon(imagenP2);
-            mostrarInventario(jugador1);
+            nombreJugadorActual.setText(jugador2.getNombre());
         }
         else{
             imagenJugadorActual.setIcon(imagenP1);
-            mostrarInventario(jugador2);
+            nombreJugadorActual.setText(jugador2.getNombre());
         }
     }
 
-
     public void mostrarInventario(Jugador jugador) {
         if (jugador.getCajaObjetos().getCaja().isEmpty()) {
-            JOptionPane.showMessageDialog(this, jugador.getNombre() + " no tiene objetos en su caja.");
+            JOptionPane.showMessageDialog(this, jugador.getNombre() + "Aquí no hay objetos...");
             return;
         }
         jugador.mostrarObjetos();
     }
+
+    public void seleccionarInventario(Jugador jugador) {
+        if (jugador.getCajaObjetos().getCaja().isEmpty()) {
+            JOptionPane.showMessageDialog(this, jugador.getNombre() + " Aquí no hay objetos...");
+            return;
+        }
+
+        SwingWorker<Objeto, Void> worker = new SwingWorker<>() {
+            protected Objeto doInBackground() {
+                CountDownLatch latch = new CountDownLatch(1);
+                
+                jugador.seleccionarObjeto(latch);
+
+                try {
+                    latch.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+
+                return jugador.objeto_a_Usar();
+            }
+
+            @Override
+            protected void done() {
+                try {
+                    
+                    Objeto objeto = get();
+    
+                    if (objeto != null) {
+                        usarCajaDelJugador(
+                            turnoJugador1 ? jugador1 : jugador2,
+                            turnoJugador1 ? jugador2 : jugador1,
+                            escopeta
+                        );
+                        if(!(objeto instanceof Adrenalina)) {
+                            jugador.getCajaObjetos().getCaja().remove(jugador.retornarPosicion());
+                        }
+                        logEventos.append(jugador.getNombre() + " usó " + objeto.getClass().getSimpleName() + "\n");
+                    } else {
+                        logEventos.append(jugador.getNombre() + " no seleccionó ningún objeto\n");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        worker.execute();
+    }
     
     public void mostrarInformacionEscopeta(JTextArea logEventos){
         logEventos.append("La escopeta se cargó con " + escopeta.getMunicion().size() + " cartuchos\n");
-        for(int i = 0; i<escopeta.getMunicion().size(); i++){
-            logEventos.append(escopeta.getMunicion().get(i).toUpperCase() + " ");
-        }
+        // for(int i = 0; i<escopeta.getMunicion().size(); i++){
+        //     logEventos.append("[" + escopeta.getMunicion().get(i).toUpperCase() + "] ");
+        // }
+        logEventos.append("\n");
     }
 
-    public void usar(Jugador jugadorActual, String objetoEnTexto, Jugador jugador2, Escopeta escopeta){
+    public void usarCajaDelJugador(Jugador jugadorActual, Jugador jugadorEnemigo, Escopeta escopeta){
         
             if (jugadorActual.objeto_a_Usar() instanceof Adrenalina) {
-                ((Adrenalina) jugadorActual.objeto_a_Usar()).usarAdrenalina(jugador2.getCajaObjetos());
+                logEventos.append("Se ha utilizado la adrenalina...\n");
+                ((Adrenalina) jugadorActual.objeto_a_Usar()).usarAdrenalina(jugadorEnemigo.getCajaObjetos());
+                ((Adrenalina) jugadorActual.objeto_a_Usar()).recibirCajaDelJugador(jugadorActual.getCajaObjetos(), jugadorActual.retornarPosicion());
             }
             else if (jugadorActual.objeto_a_Usar() instanceof Cerveza) {
-                ((Cerveza) jugadorActual.objeto_a_Usar()).UsarCerveza(escopeta);
+                logEventos.append("Se ha utilizado la cerveza...\n");
+                ((Cerveza) jugadorActual.objeto_a_Usar()).usarCerveza(escopeta, this);
+                if(escopeta.getMunicion().isEmpty()){
+                    JOptionPane.showMessageDialog(null, "Ronda terminada...");
+                    escopeta.cargarBalas();
+                    logEventos.append("Nuevas balas insertadas:\n");
+                    mostrarInformacionEscopeta(logEventos);
+                    repartirCajaJugador(jugador1);
+                    repartirCajaJugador(jugador2);
+                }
             }
             else if (jugadorActual.objeto_a_Usar() instanceof Cigarro) {
+                logEventos.append("Se ha utilizado el cigarro...\n");
                 ((Cigarro) jugadorActual.objeto_a_Usar()).utilizarCigarro(jugadorActual);
+                actualizarVidas(panelVidasJ1, jugador1.getVida());
+                actualizarVidas(panelVidasJ2, jugador2.getVida());
             }
             else if (jugadorActual.objeto_a_Usar() instanceof Esposa) {
-                ((Esposa) jugadorActual.objeto_a_Usar()).usarEsposa(jugadorActual);
+                logEventos.append("Se ha utilizado la esposa...\n");
+                ((Esposa) jugadorActual.objeto_a_Usar()).usarEsposa(jugadorEnemigo);
             }
             else if (jugadorActual.objeto_a_Usar() instanceof Lupa) {
-                ((Lupa) jugadorActual.objeto_a_Usar()).UsarLupa(escopeta);
+                logEventos.append("Se ha utilizado la lupa...\n");
+                ((Lupa) jugadorActual.objeto_a_Usar()).UsarLupa(escopeta, this);
             }
             else {
+                logEventos.append("Se ha utilizado La sierra\n");
                 ((Sierra) jugadorActual.objeto_a_Usar()).usarSierra(escopeta);
             }
     }
+
     public void repartirCajaJugador(Jugador jugador) {
         jugador.repartirObjetos();
     }
-    public void main(String[] args) {
+    public void modificarBotones(JButton boton){
+        boton.setContentAreaFilled(false);
+        boton.setBorderPainted(false);
+        boton.setFocusPainted(false);
+    }
+
+    public static void main(String[] args) {
         new Tableroo();
     }
 }
-
-
